f7b4858dec2acd1525c9e0f0bdd285a971895b88
Masdi Aditia <45525790+Masdiaditia@users.noreply.github.com>
2018-12-23 16:52:02 +0700

    Add files via upload

---
+++ Menghitung upah perjam.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j,hasil;
+    cout<<"program menghitung upah perjam\n";
+    cout<<"==============================\n";
+    cout<<"masukkan jumlah jam ="; cin >>j;
+    hasil=j*5000;
+    cout <<"jumlah upah keseluruhan = "<<hasil;
+    return 0;
+}
